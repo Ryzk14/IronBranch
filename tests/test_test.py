961638323f4461d85dba358ab08138b67abd77ff
Ryzk14 <Ryzhko14@gmail.com>
2023-06-10 01:54:28 +0300

    12

--- tests/test_test.py
+++ tests/test_test.py
@@ -28,4 +28,4 @@ def test_function6(initial_value):
     assert my_coin.set_function6() + initial_value == 13
 
 def test_function7(initial_value):
-    assert my_coin.set_function6() + initial_value == 13
+    assert my_coin.set_function6() + initial_value == 14
