279dd36850172d333ad4a5429edefb2238efa47d
Ryzk14 <Ryzhko14@gmail.com>
2023-06-12 14:32:34 +0300

    Commit

--- tests/test_pytest2/test_test4.py
+++ tests/test_pytest2/test_test4.py
@@ -16,7 +16,7 @@ def test_function3(initial_value):
 
 
 def test_function4(initial_value):
-    assert my_coin.set_function4() + initial_value == 9
+    assert my_coin.set_function4() + initial_value == 10
 
 
 def test_function5(initial_value):
