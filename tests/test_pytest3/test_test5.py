6b0f61f096a91dbf663dcb14ad4de300bbed5488
Ryzk14 <Ryzhko14@gmail.com>
2023-06-26 12:31:35 +0300

    Commit

---
+++ tests/test_pytest3/test_test5.py
@@ -0,0 +1,33 @@
+import pytest
+import test_class
+
+my_coin = test_class.Coin()
+
+
+def test_function7(initial_value):
+    assert my_coin.set_function1() + initial_value == 3
+
+
+def test_function8(initial_value):
+    ado + var == 7
+
+
+def test_function9(initial_value):
+    ado + var == 7
+
+
+def test_function10(initial_value):
+    assert my_coin.set_function4() + initial_value == 9
+
+def test_function11(initial_value):
+    assert my_coin.set_function4() + initial_value == 9
+
+def test_function12(initial_value):
+    assert my_coin.set_function5() + initial_value == 11
+
+#@pytest.mark.skip()
+def test_function13(initial_value):
+    assert my_coin.set_function6() + initial_value == 13
+
+def test_function14(initial_value):
+    assert my_coin.set_function6() + initial_value == 14
---
+++ tests/test_pytest3/test_test6.py
@@ -0,0 +1,24 @@
+import test_class
+
+my_coin = test_class.Coin()
+
+
+def test_function15(initial_value):
+    assert my_coin.set_function1() + initial_value == 3
+
+
+
+def test_function16(initial_value):
+    assert my_coin.set_function3() + float(initial_value) == 7
+
+
+def test_function17(initial_value):
+    assert my_coin.set_function4() + initial_value == 10
+
+
+def test_function18(initial_value):
+    assert my_coin.set_function5() + initial_value == 11
+
+
+def test_function19(initial_value):
+    assert my_coin.set_function6() + initial_value == 14
